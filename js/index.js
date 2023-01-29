a138581698423ca62bd5edb32e736f811c96c36c
Vladimir Patrikeyev <patrik666@gmail.com>
2023-01-29 12:46:24 +0300

    one

---
+++ js/index.js
@@ -0,0 +1,2 @@
+"use strict";var menuBtn=document.querySelector(".header__menu"),navMenu=document.querySelector(".navbar-menu");console.log(menuBtn),menuBtn.addEventListener("click",(function(e){e.preventDefault(),menuBtn.classList.toggle("header__menu--active"),navMenu.classList.toggle("navbar-menu--active")}));
+//# sourceMappingURL=../maps/index.js.map
